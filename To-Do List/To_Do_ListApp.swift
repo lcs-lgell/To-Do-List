bd82dc4a19a84d1f395128a3d226713502e3f88c
Leon Gell <lgell@lcs.on.ca>
2023-04-03 14:47:32 -0400

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Leon Gell on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
