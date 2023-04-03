b3d8bb9aa562125486abe9012ab44cbc4d6fb66b
Leon Gell <lgell@lcs.on.ca>
2023-04-03 15:08:04 -0400

    made a button

--- To-Do List/ListView.swift
+++ To-Do List/ListView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ListView: View {
+    
+    //MARK: Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView{
+            
+            VStack{
+                
+                
+                HStack{
+                    
+                    TextField("Enter a to-do item", text: Binding.constant(""))
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("ADD")
+                            .font(.caption)
+                    })
+                    
+                    
+                }
+                .padding(20)
+                
+                List{
+                    
+                    
+                }
+                
+            }
+            
+            
+        }
+       
     }
 }
 
